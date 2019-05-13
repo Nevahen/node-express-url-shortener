f341630bd08f4c84d561e6545b18db531cba7838
Oskari Nevalainen <oskari.nevalainen@gmail.com>
2019-05-13 20:07:55 +0300

    Sometimes watching your old code hurts head

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var router = express.Router()
+const router = express.Router()
 const sqlite = require('sqlite3')
 const randomid = require('./randomid');
 const validUrl = require('valid-url');
@@ -10,7 +10,7 @@ var options = {
     SHORTID_LENGTH: 6
 }
 
-db = new sqlite.Database('database.sqlite', () =>{
+db = new sqlite.Database('database.sqlite', () => {
     db.exec(`
     CREATE TABLE IF NOT EXISTS links (
         id integer PRIMARY KEY,
@@ -20,27 +20,27 @@ db = new sqlite.Database('database.sqlite', () =>{
         time text
     );
     CREATE UNIQUE INDEX IF NOT EXISTS key ON links('key');
-    `,(err)=>{
-        if(err){
+    `,(err)=> {
+        if(err) {
             console.log(err);
         }
     })
 })
 
-router.use(express.static('static'))
+router.use(express.static('static'));
 
 router.get('/notfound', (req, res) => {
     res.status(404).sendFile(path.join(__dirname+'/static/notfound.html'));
-})
+});
 
 router.get('/:id', (req, res)=> {
     const id = req.params.id;
-    db.get("SELECT * from links WHERE key = ?", id,function(err,row){
-        if(err){
-            console.log(err)
+    db.get("SELECT * from links WHERE key = ?", id,function(err,row) {
+        if(err) {
+            console.log(err);
             res.status(500).json({error:"Internal server error."})
         }
-        if(row === undefined){
+        if(row === undefined) {
             res.redirect('/notfound');
         }
         else{
@@ -49,11 +49,11 @@ router.get('/:id', (req, res)=> {
     })
 })
 
-router.post('/', async (req, res) =>{
+router.post('/', async (req, res) => {
     
     // Checks
 
-    if(!req.body.key){
+    if(!req.body.key) {
         let payload = {
             error: "No URL provided"
         }
@@ -61,17 +61,17 @@ router.post('/', async (req, res) =>{
         return;
     }
 
-    if(!validUrl.isWebUri(req.body.key)){
+    if(!validUrl.isWebUri(req.body.key)) {
         let payload = {error:"That wasn't a valid URL"}
-        res.status(400).json(payload).end()
-        return
+        res.status(400).json(payload).end();
+        return;
     }
 
-    id = await createRecord(req.body.key,req.ip)
+    const id = await createRecord(req.body.key,req.ip);
     
-    if(id === undefined){
-        res.status(500).json({error:"Unexpected error"})
-        return
+    if (id === undefined) {
+        res.status(500).json({ error:"Unexpected error" });
+        return;
     }
 
     // Sending created shortid
@@ -83,12 +83,12 @@ router.post('/', async (req, res) =>{
     res.json(payload);
 })
 
-async function createRecord(target,ip){
-    key = randomid.randomId(options.SHORTID_LENGTH)
-    exists = await checkExists(key)
+async function createRecord(target,ip) {
+    key = randomid.randomId(options.SHORTID_LENGTH);
+    exists = await checkExists(key);
 
-    if(exists){
-        return undefined
+    if (exists) {
+        return undefined;
     }
 
     linkObject = {
@@ -99,32 +99,31 @@ async function createRecord(target,ip){
     }
 
     db.run('INSERT into links (target,key,time,creator) VALUES ($target,$key,$time,$creator);', linkObject, (err) => {
-        if(err){
-            console.log(err)
+        if(err) {
+            console.log(err);
         }
     })
-    return linkObject.$key
+    return linkObject.$key;
 }
 
 
-function checkExists(key){
-
+function checkExists(key) {
     return new Promise((resolve, reject) => {
         a = db.get('SELECT * from links where key = ?', key, (err,row) => {
             if(err){
             }
 
-            if(row === undefined){
+            if(!row){
                 resolve(false)
             }
             else {
                 resolve(true);
             }
-        })
-    })
+        });
+    });
 }
 
-router.use((req, res)=>{
+router.use((req, res) => {
     res.redirect('/notfound');
 })
 
